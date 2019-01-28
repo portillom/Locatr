fb2ec1edda3aa5ec3c2e533ec0722123c6dd004b
USER <portillom@g.cofc.edu>
2019-01-27 16:51:36 -0800

    Initial commit

---
+++ app/src/main/java/com/michaelportillo/android/locatr/FlickrFetchr.java
@@ -0,0 +1,149 @@
+package com.michaelportillo.android.locatr;
+
+import android.net.Uri;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by USER on 1/7/19.
+ */
+
+public class FlickrFetchr {
+
+    private static final String TAG = "FlickrFetchr";
+
+    private static final String API_KEY = "898d96ed30626583b621b29a1d49f1e8";
+    private static final String FETCH_RECENTS_METHOD = "flickr.photos.getRecent";
+    private static final String SEARCH_METHOD = "flickr.photos.search";
+    private static final Uri ENDPOINT = Uri
+            .parse("https://api.flickr.com/services/rest/")
+            .buildUpon()
+            .appendQueryParameter("api_key", API_KEY)
+            .appendQueryParameter("format", "json")
+            .appendQueryParameter("nojsoncallback", "1")
+            .appendQueryParameter("extras", "url_s")
+            .build();
+
+    /**
+     * This code creates a URL object from a string, then it calls openConnection() to create a
+     * connection object pointed at the URL. URL.connection() returns a URLConnection., but since
+     * it's connecting to an http URL, cast it to HttpURLConnection.
+     *
+     * This gives you HTTP-specific interfaces for working with request methods, response codes,
+     * streaming methods, and more.
+     * @param urlSpec
+     * @return
+     * @throws IOException
+     */
+    public byte[] getUrlBytes(String urlSpec) throws IOException{
+        URL url = new URL(urlSpec);
+        HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+
+        try{
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            InputStream in = connection.getInputStream();
+
+            if (connection.getResponseCode() != HttpURLConnection.HTTP_OK){
+                throw new IOException(connection.getResponseMessage() +
+                ": with " + urlSpec);
+            }
+
+            int bytesRead = 0;
+            byte[] buffer = new byte[1024];
+            while ((bytesRead = in.read(buffer)) > 0){
+                out.write(buffer, 0, bytesRead);
+            }
+            out.close();
+            return out.toByteArray();
+        } finally {
+            connection.disconnect();
+        }
+    }
+
+    public String getUrlString(String urlSpec) throws IOException {
+        return new String(getUrlBytes(urlSpec));
+    }
+
+    public List<GalleryItem> fetchRecentPhotos(){
+        String url = buildUrl(FETCH_RECENTS_METHOD, null);
+        return downloadGalleryItems(url);
+    }
+
+    public List<GalleryItem> searchPhotos(String query) {
+        String url = buildUrl(SEARCH_METHOD, query);
+        return downloadGalleryItems(url);
+    }
+
+    /**
+     * Using a Uri.builder to build the complete URL for Flickr API request.
+     */
+    private List<GalleryItem> downloadGalleryItems(String url){
+
+        List<GalleryItem> items = new ArrayList<>();
+
+        try{
+            String jsonString = getUrlString(url);
+            Log.i(TAG, "Received JSON: " + jsonString);
+            JSONObject jsonBody = new JSONObject(jsonString);
+            parseItems(items, jsonBody);
+        } catch (IOException ioe){
+            Log.e(TAG, "Failed to fetch items", ioe);
+        } catch (JSONException je){
+            Log.e(TAG, "Failed to parse JSON", je);
+        }
+
+        return items;
+    }
+
+    private String buildUrl(String method, String query){
+        Uri.Builder uriBuilder = ENDPOINT.buildUpon()
+                .appendQueryParameter("method", method);
+
+        if (method.equals(SEARCH_METHOD)) {
+            uriBuilder.appendQueryParameter("text", query);
+        }
+
+        return uriBuilder.build().toString();
+    }
+
+    /**
+     * This code uses convenience methods such as getJSONObject(String name) and getJSONArray(String name)
+     * to navigate the JSONObject hierarchy.
+     * @param items
+     * @param jsonBody
+     * @throws IOException
+     * @throws JSONException
+     */
+    private void parseItems(List<GalleryItem> items, JSONObject jsonBody)
+        throws IOException, JSONException {
+        JSONObject photosJsonObject = jsonBody.getJSONObject("photos");
+        JSONArray photoJsonArray = photosJsonObject.getJSONArray("photo");
+
+        for (int i = 0; i < photoJsonArray.length(); i++){
+            JSONObject photoJsonObject = photoJsonArray.getJSONObject(i);
+
+            GalleryItem item = new GalleryItem();
+            item.setId(photoJsonObject.getString("id"));
+            item.setCaption(photoJsonObject.getString("title"));
+
+            if (!photoJsonObject.has("url_s")){
+                continue;
+            }
+
+            item.setUrl(photoJsonObject.getString("url_s"));
+            item.setOwner(photoJsonObject.getString("owner"));
+            items.add(item);
+        }
+    }
+}
---
+++ app/src/main/java/com/michaelportillo/android/locatr/GalleryItem.java
@@ -0,0 +1,62 @@
+package com.michaelportillo.android.locatr;
+
+import android.net.Uri;
+
+/**
+ * Created by USER on 1/8/19.
+ */
+
+public class GalleryItem {
+    private String mCaption;
+    private String mId;
+    private String mUrl;
+    private String mOwner;
+
+    public void setCaption(String caption) {
+        mCaption = caption;
+    }
+
+    public String getCaption() {
+        return mCaption;
+    }
+
+    public String getId() {
+        return mId;
+    }
+
+    public void setId(String id) {
+        mId = id;
+    }
+
+    public String getUrl() {
+        return mUrl;
+    }
+
+    public void setUrl(String url) {
+        mUrl = url;
+    }
+
+    public String getOwner() {
+        return mOwner;
+    }
+
+    public void setOwner(String owner) {
+        mOwner = owner;
+    }
+
+    public Uri getPhotoPageUri() {
+        return Uri.parse("http://www.flickr.com/photos/")
+                .buildUpon()
+                .appendPath(mOwner)
+                .appendPath(mId)
+                .build();
+    }
+
+
+
+
+    @Override
+    public String toString(){
+        return mCaption;
+    }
+}
