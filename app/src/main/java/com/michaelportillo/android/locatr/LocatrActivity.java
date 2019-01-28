a51e37be1126feb794489236c580904d23983e4f
USER <portillom@g.cofc.edu>
2019-01-27 16:59:40 -0800

    Adding search button text & Setting up Locatr's menu

--- app/src/main/java/com/michaelportillo/android/locatr/LocatrActivity.java
+++ app/src/main/java/com/michaelportillo/android/locatr/LocatrActivity.java
@@ -8,6 +8,6 @@ public class LocatrActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_locatr);
+        setContentView(R.layout.fragment_locatr);
     }
 }
