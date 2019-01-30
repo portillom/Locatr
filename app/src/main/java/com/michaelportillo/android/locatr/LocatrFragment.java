8706d1a4a565fb0810d24d9045c18c4b790793de
USER <portillom@g.cofc.edu>
2019-01-30 12:25:38 -0800

    Sending LocationRequest

--- app/src/main/java/com/michaelportillo/android/locatr/LocatrFragment.java
+++ app/src/main/java/com/michaelportillo/android/locatr/LocatrFragment.java
@@ -1,7 +1,9 @@
 package com.michaelportillo.android.locatr;
 
+import android.location.Location;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -11,6 +13,7 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 
@@ -21,6 +24,7 @@ import java.util.zip.Inflater;
  */
 
 public class LocatrFragment extends Fragment {
+    private static final String TAG = "LocatrFragment";
     private ImageView mImageView;
     private GoogleApiClient mClient;
 
@@ -88,5 +92,12 @@ public class LocatrFragment extends Fragment {
         request.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
         request.setNumUpdates(1);
         request.setInterval(0);
+        LocationServices.FusedLocationApi
+                .requestLocationUpdates(mClient, request, new LocationListener() {
+                    @Override
+                    public void onLocationChanged(Location location) {
+                        Log.i(TAG, "Got a fix: " + location);
+                    }
+                });
     }
 }
