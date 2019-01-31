74f993c5b8840c2a0279d61710cbf61e612d22c9
USER <portillom@g.cofc.edu>
2019-01-31 14:00:28 -0800

    Responding to permissions request result

--- app/src/main/java/com/michaelportillo/android/locatr/LocatrFragment.java
+++ app/src/main/java/com/michaelportillo/android/locatr/LocatrFragment.java
@@ -111,6 +111,19 @@ public class LocatrFragment extends Fragment {
         }
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                                           int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_LOCATION_PERMISSIONS:
+                if (hasLocationPermission()) {
+                    findImage();
+                }
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
     private void findImage() {
         LocationRequest request = LocationRequest.create();
         request.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
